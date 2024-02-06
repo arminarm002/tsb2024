d8df38f1ff32c5cc8b350720b4611c38d2a90277
arminarm002 <alarm.1993@gmail.com>
2024-02-06 16:38:58 +0700

    16.38

--- pages/talk.php
+++ pages/talk.php
@@ -20,14 +20,41 @@ include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/connectdb.php');
 
   <div class="container">
     <h1 class="card-title centerer mt-3">Plenary Talks & Speakers</h1>
-    <h4 style="text-align: center;">Plenary Talks</h4>
-    <img src="/spc2024/img/soon.png" style="display:block; margin:auto; width:50%;">
-    <hr>
-    <h4 style="text-align: center;">Keynote Lectures</h4>
-    <img src="/spc2024/img/soon.png" style="display:block; margin:auto; width:50%;">
-    <hr>
-    <h4 style="text-align: center;">Invited Speakers</h4>
-    <img src="/spc2024/img/soon.png" style="display:block; margin:auto; width:50%;">
+    <h2 style="text-align: center;">Plenary Talks</h2>
+    <div style="text-align: center;">
+      <img src="/spc2024/img/speaker/pss01.png" class="w-25">
+      <h2 style="color:blue">Prof. Yifang WANG</h2>
+      <h5 style="color:blue">Director of Institute of High Energy Physics of Chinese Academy of Sciences</h5>
+      <p class="color-black" style="text-align:justify;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; He is an author on more
+        than 500 scientific papers on neutrino physics, e+ e- collision physics, cosmic rays and astrophysics,
+        detector design and construction, and methods for data analysis, covering his work on the L3, AMS, Palo Verde,
+        KamLAND and BES experiments. He led the design and construction of the BESIII detector at the Beijing
+        Electron-Positron Collider, and the Daya Bay Reactor Neutrino Experiment, which precisely measured the
+        neutrino mixing angle theta13. He proposed the idea of the Circular Electron-Positron Collider (CEPC) followed
+        by a proton collider (SPPC) as a possible future step for particle physics. As the spokesperson of Jiangmen
+        Underground Neutrino Observatory(JUNO), he is leading the design and construction of the project.
+        He was awarded the W. K. H. Panofsky Prize in 2013, the 20th Nikkei Asia Prize in 2015, the Breakthrough Prize
+        in Fundamental Physics in 2016 , the Bruno Pontecorvo Prize in 2016 and the Future Science Prize in 2019.
+      </p>
+    </div>
+    <div style="text-align: center;">
+      <img src="/spc2024/img/speaker/pss02.png" class="w-25">
+      <h2 style="color:blue">Prof. Albrecht Karle</h2>
+      <h5 style="color:blue">full professor and former Chair of the Department of Physics at the University of Wisconsin-Madison.</h5>
+      <p class="color-black" style="text-align:justify;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; His primary research is about our understanding of the origin of energetic cosmic rays and neutrinos using the IceCube Neutrino Observatory, where he is Associate Director for Science and Instrumentation.
+He is a Co-Principal Investigator of IceCube Neutrino Observatory and Principal Investigator of the IceCube Upgrade construction project, an extension of IceCube for precision neutrino physics and astrophysics.
+Karle analyzes data focusing on the search for sources of neutrinos in the Southern Sky using veto techniques to reject backgrounds.  Karle is also involved in advancing the radio detection method of cosmic neutrinos using the glacial ice at the South Pole and in Greenland. 
+He is also working on developing the conceptual design of the next-generation large extension IceCube-Gen2.
+
+      </p>
+    </div>
+  </div>
+  <hr>
+  <h2 style="text-align: center;">Keynote Lectures</h2>
+  <img src="/spc2024/img/soon.png" style="display:block; margin:auto; width:50%;">
+  <hr>
+  <h2 style="text-align: center;">Invited Speakers</h2>
+  <img src="/spc2024/img/soon.png" style="display:block; margin:auto; width:50%;">
   </div>
 
   <?php
