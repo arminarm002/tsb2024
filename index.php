3fb81c8f1b59dee1704bf0b13146c3d740cb01f0
arminarm002 <alarm.1993@gmail.com>
2024-02-13 12:58:22 +0700

    12.58

--- index.php
+++ index.php
@@ -90,65 +90,65 @@ include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/connectdb.php');
 
         <div class="cards mb-3 mt-5" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;padding-bottom: 15px;">
           <h1 class="card-title centerer" style="color: #7b7b7b;padding-top: 15px;">Announcement</h1>
-          <div class="image">
-            <div class="row" style="margin: 15px auto;">
-              <div class="col-sm-12 col-md-6 col-lg-4 centerer mt-3">
-                <div class="card border border-warning rounded" style="width: 28rem;"><!-- 18rem -->
-                  <img class="border-bottom border-warning" src="/spc2024/img/news/speaker1.jpg"
-                    style="width:100%; ">
-                  <div class="card-body">
-                    <h5 class="card-title"> 12 February 2024</h5>
-                    <p class="card-text" style="text-align:justify;">The updated list of plenary talks includes presentations by Prof. Yifang Wang And Prof. Albrecht Karle</p>
-                    <a href="/spc2024/pages/talk.php" class="btn btn-warning">
-                      <h6 class="card-title">-- More --</h6>
-                    </a>
-                  </div>
+          <div class="row" style="margin: 15px auto;">
+            <div class="col-sm-12 col-md-6 col-lg-4 centerer mt-3">
+              <div class="card border border-warning rounded" style="width: 28rem;"><!-- 18rem -->
+                <img class="border-bottom border-warning" src="/spc2024/img/news/speaker1.jpg" style="width:100%; ">
+                <div class="card-body">
+                  <h5 class="card-title"> 12 February 2024</h5>
+                  <p class="card-text" style="text-align:justify;">The updated list of plenary talks includes
+                    presentations by Prof. Yifang Wang And Prof. Albrecht Karle</p>
+                  <a href="/spc2024/pages/talk.php" class="btn btn-warning">
+                    <h6 class="card-title">-- More --</h6>
+                  </a>
                 </div>
               </div>
-              <div class="col-sm-12 col-md-6 col-lg-4 centerer mt-3">
-                <div class="card border border-warning rounded" style="width: 28rem;"><!-- 18rem -->
-                  <img class="border-bottom border-warning" src="/spc2024/img/news/nobel.jpg" style="width:100%; ">
-                  <div class="card-body">
-                    <h5 class="card-title"> 3 October 2023</h5>
-                    <p class="card-text" style="text-align:justify;">3 scientists awarded 2023 Nobel Prize in Physics
-                      for
-                      use of light to study
-                      electrons</p>
-                    <a href="https://news.cgtn.com/news/2023-10-03/3-scientists-awarded-2023-Nobel-Prize-in-Physics-1nBg0xV00aA/index.html"
-                      class="btn btn-warning" target="_blank">
-                      <h6 class="card-title">-- More --</h6>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-12 col-md-6 col-lg-4 centerer mt-3">
-                <div class="card border border-warning rounded" style="width: 28rem;"><!-- 18rem -->
-                  <img class="border-bottom border-warning" src="/spc2024/img/news/flag.jpg" style="width:100%; ">
-                  <div class="card-body">
-                    <h5 class="card-title">17 June 2023</h5>
-                    <p class="card-text" style="text-align:justify;">ภาควิชาฟิสิกส์
-                      คณะวิทยาศาสตร์และวิทยาลัยเทคโนโลยีและนวัตกรรมวัสดุ
-                      รับมอบธงร่วมเป็นเจ้าภาพจัดงานประชุมวิชาการ Siam Physics Congress ครั้งที่ 19
-                      ซึ่งจะจัดขึ้นระหว่างวันที่ 5-7 มิถุนายน ปี 2567</p>
-                    <a href="https://www.facebook.com/profile/100063613568041/search/?q=%E0%B8%A3%E0%B8%B1%E0%B8%9A%E0%B8%A1%E0%B8%AD%E0%B8%9A%E0%B8%98%E0%B8%87"
-                      class="btn btn-warning" target="_blank">
-                      <h6 class="card-title">-- More --</h6>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              </iv>
             </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 centerer mt-3">
+              <div class="card border border-warning rounded" style="width: 28rem;"><!-- 18rem -->
+                <img class="border-bottom border-warning" src="/spc2024/img/news/nobel.jpg" style="width:100%; ">
+                <div class="card-body">
+                  <h5 class="card-title"> 3 October 2023</h5>
+                  <p class="card-text" style="text-align:justify;">3 scientists awarded 2023 Nobel Prize in Physics
+                    for
+                    use of light to study
+                    electrons</p>
+                  <a href="https://news.cgtn.com/news/2023-10-03/3-scientists-awarded-2023-Nobel-Prize-in-Physics-1nBg0xV00aA/index.html"
+                    class="btn btn-warning" target="_blank">
+                    <h6 class="card-title">-- More --</h6>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 centerer mt-3">
+              <div class="card border border-warning rounded" style="width: 28rem;"><!-- 18rem -->
+                <img class="border-bottom border-warning" src="/spc2024/img/news/flag.jpg" style="width:100%; ">
+                <div class="card-body">
+                  <h5 class="card-title">17 June 2023</h5>
+                  <p class="card-text" style="text-align:justify;">ภาควิชาฟิสิกส์
+                    คณะวิทยาศาสตร์และวิทยาลัยเทคโนโลยีและนวัตกรรมวัสดุ
+                    รับมอบธงร่วมเป็นเจ้าภาพจัดงานประชุมวิชาการ Siam Physics Congress ครั้งที่ 19
+                    ซึ่งจะจัดขึ้นระหว่างวันที่ 5-7 มิถุนายน ปี 2567</p>
+                  <a href="https://www.facebook.com/profile/100063613568041/search/?q=%E0%B8%A3%E0%B8%B1%E0%B8%9A%E0%B8%A1%E0%B8%AD%E0%B8%9A%E0%B8%98%E0%B8%87"
+                    class="btn btn-warning" target="_blank">
+                    <h6 class="card-title">-- More --</h6>
+                  </a>
+                </div>
+              </div>
+            </div>
+            </iv>
           </div>
         </div>
+      </div>
+  </div>
 
 
-        <?php
-        include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/components/footer.php');
-        include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/script/script.php');
-        include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/script/countdown.php');
-        include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/script/messenger.php');
-        ?>
+  <?php
+  include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/components/footer.php');
+  include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/script/script.php');
+  include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/script/countdown.php');
+  include($_SERVER['DOCUMENT_ROOT'] . '/spc2024/script/messenger.php');
+  ?>
 
 </body>
 
