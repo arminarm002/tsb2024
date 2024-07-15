6b1d48e06a8e5f096470bc6c62a3954c5138b918
arminarm002 <alarm.1993@gmail.com>
2024-07-15 16:38:03 +0700

    ขยายเวลาหน้า important date

--- pages/date.php
+++ pages/date.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include($_SERVER['DOCUMENT_ROOT'] . '/db/connectdb.php');
+include ($_SERVER['DOCUMENT_ROOT'] . '/db/connectdb.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,30 +15,43 @@ include($_SERVER['DOCUMENT_ROOT'] . '/db/connectdb.php');
 
 <body>
   <?php
-  include($_SERVER['DOCUMENT_ROOT'] . '/components/navbar.php');
+  include ($_SERVER['DOCUMENT_ROOT'] . '/components/navbar.php');
   ?>
-<a href="https://www.messenger.com/t/279532238569467/?messaging_source=source%3Apages%3Amessage_shortlink&source_id=1441792&recurring_notification=0" target="_blank">
+  <a href="https://www.messenger.com/t/279532238569467/?messaging_source=source%3Apages%3Amessage_shortlink&source_id=1441792&recurring_notification=0"
+    target="_blank">
     <img src="/../img/messenger.png" class="img-fluid messenger">
   </a>
   <div class="container">
-  <h1 class="card-title centerer mt-3">Important Dates & Overall Program</h1>
+    <h1 class="card-title centerer mt-3">Important Dates & Overall Program</h1>
     <table class="table tb-stip table-striped table-hover mt-5" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
-      <tbody style="font-size: 2vw;">
+      <tbody style="font-size: 1.6vw;">
         <tr>
           <td>Registration & Submissions opens</td>
           <td>May 15, 2024</td>
         </tr>
         <tr>
           <td>Abstract submission deadline</td>
-          <td>July 15, 2024</td>
+          <td>
+            <span class="redmidline" style="color:#f78787">July 15, 2024</span>
+            <span class="red"> 31 July 2024</span>
+          </td>
         </tr>
         <tr>
           <td>Notification of Abstract acceptance</td>
           <td>August 1-15, 2024</td>
         </tr>
         <tr>
-          <td>Full paper submission deadline</td>
-          <td>August 16, 2024</td>
+          <td>
+            Full paper submission deadline <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- E-Proceeding<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- CAST, SSSTJ, TJNN
+          </td>
+          <td><br>
+            <span class="redmidline" style="color:#f78787">August 16, 2024</span>
+            <span class="red">August 30 2024</span><br>
+            <span class="redmidline" style="color:#f78787">November 14, 2024</span>
+            <span class="red">December 16 July 2024</span>
+          </td>
         </tr>
         <tr>
           <td>Notification of Full paper acceptance</td>
@@ -50,7 +63,10 @@ include($_SERVER['DOCUMENT_ROOT'] . '/db/connectdb.php');
         </tr>
         <tr>
           <td>Late registration deadline</td>
-          <td>October 16, 2024</td>
+          <td>
+            <span class="redmidline" style="color:#f78787">October 16, 2024</span>
+            <span class="red"> 1 November 2024</span>
+          </td>
         </tr>
         <tr>
           <td>TSB2024 Conference</td>
@@ -67,7 +83,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/db/connectdb.php');
         </tr>
       </thead>
       <tbody style="font-size: 2vw;">
-      <tr>
+        <tr>
           <td></td>
           <td></td>
         </tr>
@@ -117,9 +133,9 @@ include($_SERVER['DOCUMENT_ROOT'] . '/db/connectdb.php');
   </div>
 
   <?php
-  include($_SERVER['DOCUMENT_ROOT'] . '/components/footer.php');
-  include($_SERVER['DOCUMENT_ROOT'] . '/script/script.php');
-  
+  include ($_SERVER['DOCUMENT_ROOT'] . '/components/footer.php');
+  include ($_SERVER['DOCUMENT_ROOT'] . '/script/script.php');
+
   ?>
 </body>
 
